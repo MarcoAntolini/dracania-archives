4773161e00465821456eac2ae2d253b7f7079ec2
MarcoAntolini <marco.antolini2001@gmail.com>
2023-10-26 13:17:46 +0200

    update gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-32 gap-16">
+		<main className="flex min-h-screen flex-col items-center justify-between p-32 gap-12">
 			<h1 className="font-extrabold text-6xl">DSO Database</h1>
 			<h2 className="text-2xl">⚠️ Currently work in progress... ⚠️</h2>
 		</main>
