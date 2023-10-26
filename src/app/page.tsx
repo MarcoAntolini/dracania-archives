6fd946c0f375edeb49e6b01bf2fc389b6fd50013
MarcoAntolini <marco.antolini2001@gmail.com>
2023-10-26 13:10:50 +0200

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center justify-between p-32 gap-16">
 			<h1 className="font-extrabold text-6xl">DSO Database</h1>
+			<h2 className="text-2xl">⚠️ Currently work in progress... ⚠️</h2>
 		</main>
 	);
 }
