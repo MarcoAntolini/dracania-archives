f75531485b3e8070c4b7cdb67ae5545253f78202
MarcoAntolini <marco.antolini2001@gmail.com>
2023-10-27 02:37:24 +0200

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="en">
 			<head>
-				<link rel="icon" href="favicon.ico" />
+				<link rel="icon" href="https://dso-database-dun.vercel.app/favicon.ico" />
 			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
