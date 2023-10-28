611abf6dfa50cc84af443ca1c044725947339845
Marco Antolini <marco.antolini2001@gmail.com>
2023-10-29 00:53:11 +0200

    add all item types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-32 gap-8 bg-slate-700">
+		<main className="flex min-h-screen flex-col items-center p-32 gap-20 bg-slate-700">
 			<h1 className="font-extrabold text-6xl">DSO Database</h1>
 			<h2 className="text-2xl">⚠️ Currently work in progress... ⚠️</h2>
 		</main>
