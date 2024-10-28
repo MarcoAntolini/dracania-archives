1d0782267789d99ff204e6f39a1b50bd2a5a5fd5
Marco Antolini <marco.antolini2001@gmail.com>
2024-10-28 04:20:41 +0100

    commit config files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack: (config) => {
+		config.cache = false;
+		return config;
+	},
 	images: {
 		remotePatterns: [{ protocol: "https", hostname: "api.dracania-archives.com", pathname: "/images/**", port: "" }],
 	},
