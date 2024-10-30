f9d01d71981610403afd4cd0b8df0539441355c1
Marco Antolini <marco.antolini2001@gmail.com>
2024-10-30 16:01:57 +0100

    add new events

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -1,5 +1,14 @@
-type Role = "Developer" | "Database Contributor" | "Donator" | "New Features Suggestor" | "Tester";
+type Role = "Developer" | "Database Contributor" | "Donator" | "Idea Generator" | "Beta Tester";
 
-const contributors: { name: string; role: Role | Role[] }[] = [];
+const contributors: { name: string; role: Role | Role[] }[] = [
+	{
+		name: "Cezara",
+		role: "Database Contributor",
+	},
+	{
+		name: "Shiver",
+		role: "Database Contributor",
+	},
+];
 
 export default contributors;
--- src/data/real-time/events.ts
+++ src/data/real-time/events.ts
@@ -1,110 +1,249 @@
+const colors = {
+	newMoon: "#daf54e",
+	inventoryExtensionSale: "#9b5d00",
+	lockerExtensionSale: "#9b7336",
+	premiumDay: "#c800ff",
+	gnobsNewestOffers: "#ffe76d",
+	terrifyingShadows: "#946dff",
+	timeOfWisdom: "#5cc1ff",
+	gemsSale: "#3d6acb",
+	timeOfSolidarity: "#48ff48",
+	fullMoon: "#f84f84",
+	dyesSale: "#eaa6ff",
+	desertOfEssences: "#ff6f6f",
+	lootersFortune: "#00a624",
+	ghostFestival: "#a7a8a9",
+	timeOfInsight: "#e58a13",
+	liveStream: "#ffffff",
+	descentIntoTheAncestralRuins: "#ff4500",
+};
+
+// Gold - #ffd700
+// Electric Blue - #7df9ff
+// Crimson Red - #dc143c
+// Royal Purple - #7851a9
+// Emerald Green - #50c878
+// Goldenrod - #daa520
+// Teal - #008080
+// Coral Pink - #ff6f61
+// Bright Turquoise - #08e8de
+// Lemon Yellow - #fff44f
+
 export const events = [
 	{
 		title: "🕸️ New Moon",
 		startDate: new Date("2024-10-01"),
 		endDate: new Date("2024-10-03"),
-		color: "#daf54e",
+		color: colors.newMoon,
 	},
 	{
 		title: "📦 Inventory Extension Sale",
 		startDate: new Date("2024-10-01"),
 		endDate: new Date("2024-10-07"),
-		color: "#9b5d00",
+		color: colors.inventoryExtensionSale,
 	},
 	{
 		title: "🔒 Locker Extension Sale",
 		startDate: new Date("2024-10-01"),
 		endDate: new Date("2024-10-07"),
-		color: "#9b7336",
+		color: colors.lockerExtensionSale,
 	},
 	{
 		title: "🎇 Premium Day",
 		startDate: new Date("2024-10-04"),
 		endDate: new Date("2024-10-06"),
-		color: "#c800ff",
+		color: colors.premiumDay,
 	},
 	{
 		title: "🧌 Gnob's Newest Offers (New Moon offers)",
 		startDate: new Date("2024-10-04"),
 		endDate: new Date("2024-10-06"),
-		color: "#ffe76d",
+		color: colors.gnobsNewestOffers,
 	},
 	{
 		title: "👹 Terrifying Shadows - A New Era",
 		startDate: new Date("2024-10-04"),
 		endDate: new Date("2024-10-10"),
-		color: "#946dff",
+		color: colors.terrifyingShadows,
 	},
 	{
 		title: "📖 Time of Wisdom (100%)",
 		startDate: new Date("2024-10-04"),
 		endDate: new Date("2024-10-10"),
-		color: "#5cc1ff",
+		color: colors.timeOfWisdom,
 	},
 	{
 		title: "💎 Gems Sale",
 		startDate: new Date("2024-10-08"),
 		endDate: new Date("2024-10-14"),
-		color: "#3d6acb",
+		color: colors.gemsSale,
 	},
 	{
 		title: "🧌 Gnob's Newest Offers (Terrifying Shadows - A New Era offers)",
 		startDate: new Date("2024-10-11"),
 		endDate: new Date("2024-10-13"),
-		color: "#ffe76d",
+		color: colors.gnobsNewestOffers,
 	},
 	{
 		title: "💪 Time of Solidarity (100%)",
 		startDate: new Date("2024-10-11"),
 		endDate: new Date("2024-10-17"),
-		color: "#48ff48",
+		color: colors.timeOfSolidarity,
 	},
 	{
 		title: "🐺 Full Moon",
 		startDate: new Date("2024-10-15"),
 		endDate: new Date("2024-10-17"),
-		color: "#f84f84",
+		color: colors.fullMoon,
 	},
 	{
 		title: "🎨 Dyes Sale",
 		startDate: new Date("2024-10-15"),
 		endDate: new Date("2024-10-21"),
-		color: "#eaa6ff",
+		color: colors.dyesSale,
 	},
 	{
 		title: "🧌 Gnob's Newest Offers (Full Moon offers)",
 		startDate: new Date("2024-10-18"),
 		endDate: new Date("2024-10-20"),
-		color: "#ffe76d",
+		color: colors.gnobsNewestOffers,
 	},
 	{
 		title: "🌪️ Desert of Essences",
 		startDate: new Date("2024-10-18"),
 		endDate: new Date("2024-10-24"),
-		color: "#ff6f6f",
+		color: colors.desertOfEssences,
 	},
 	{
 		title: "🍀 Looter's Fortune (Equipment items)",
 		startDate: new Date("2024-10-18"),
 		endDate: new Date("2024-10-24"),
-		color: "#00a624",
+		color: colors.lootersFortune,
 	},
 	{
 		title: "👻 Ghost Festival",
 		startDate: new Date("2024-10-24"),
 		endDate: new Date("2024-11-15"),
-		color: "#a7a8a9",
+		color: colors.ghostFestival,
 	},
 	{
 		title: "💡 Time of Insight (50%)",
 		startDate: new Date("2024-10-25"),
 		endDate: new Date("2024-10-31"),
-		color: "#e58a13",
+		color: colors.timeOfInsight,
 	},
 	{
 		title: "🎥 Live Stream",
 		startDate: new Date("2024-10-25"),
 		endDate: new Date("2024-10-25"),
-		color: "#ffffff",
+		color: colors.liveStream,
+	},
+	{
+		title: "🕸️ New Moon",
+		startDate: new Date("2024-11-01"),
+		endDate: new Date("2024-11-03"),
+		color: colors.newMoon,
+	},
+	{
+		title: "🎇 Premium Day",
+		startDate: new Date("2024-11-01"),
+		endDate: new Date("2024-11-03"),
+		color: colors.premiumDay,
+	},
+	{
+		title: "🧌 Gnob's Newest Offers (Pet Harpy offers)",
+		startDate: new Date("2024-11-01"),
+		endDate: new Date("2024-11-07"),
+		color: colors.gnobsNewestOffers,
+	},
+	{
+		title: "📖 Time of Wisdom (100%)",
+		startDate: new Date("2024-11-01"),
+		endDate: new Date("2024-11-07"),
+		color: colors.timeOfWisdom,
+	},
+	{
+		title: "🪦 Ghost Festival - Return of the Dead",
+		startDate: new Date("2024-11-04"),
+		endDate: new Date("2024-11-15"),
+		color: colors.ghostFestival,
+	},
+	{
+		title: "👹 Terrifying Shadows - A New Era",
+		startDate: new Date("2024-11-06"),
+		endDate: new Date("2024-11-12"),
+		color: colors.terrifyingShadows,
+	},
+	{
+		title: "🧌 Gnob's Newest Offers (Pet M'Edusa offers)",
+		startDate: new Date("2024-11-08"),
+		endDate: new Date("2024-11-10"),
+		color: colors.gnobsNewestOffers,
+	},
+	{
+		title: "💪 Time of Solidarity (100%)",
+		startDate: new Date("2024-11-08"),
+		endDate: new Date("2024-11-14"),
+		color: colors.timeOfSolidarity,
+	},
+	{
+		title: "🧌 Gnob's Newest Offers (Pet Heredur offers)",
+		startDate: new Date("2024-11-11"),
+		endDate: new Date("2024-11-13"),
+		color: colors.gnobsNewestOffers,
+	},
+	{
+		title: "🧌 Gnob's Newest Offers (Pet Bearach offers)",
+		startDate: new Date("2024-11-14"),
+		endDate: new Date("2024-11-16"),
+		color: colors.gnobsNewestOffers,
+	},
+	{
+		title: "🕯️ Descent Into the Ancestral Ruins",
+		startDate: new Date("2024-11-14"),
+		endDate: new Date("2024-11-28"),
+		color: colors.descentIntoTheAncestralRuins,
+	},
+	{
+		title: "🐺 Full Moon",
+		startDate: new Date("2024-11-15"),
+		endDate: new Date("2024-11-17"),
+		color: colors.fullMoon,
+	},
+	{
+		title: "🍀 Looter's Fortune (Equipment items)",
+		startDate: new Date("2024-11-15"),
+		endDate: new Date("2024-11-21"),
+		color: colors.lootersFortune,
+	},
+	{
+		title: "🧌 Gnob's Newest Offers (Pet Nefertari offers)",
+		startDate: new Date("2024-11-17"),
+		endDate: new Date("2024-11-19"),
+		color: colors.gnobsNewestOffers,
+	},
+	{
+		title: "🌪️ Desert of Essences",
+		startDate: new Date("2024-11-20"),
+		endDate: new Date("2024-11-26"),
+		color: colors.desertOfEssences,
+	},
+	{
+		title: "🧌 Gnob's Newest Offers (Ghost Festival offers)",
+		startDate: new Date("2024-11-20"),
+		endDate: new Date("2024-11-22"),
+		color: colors.gnobsNewestOffers,
+	},
+	{
+		title: "💡 Time of Insight (50%)",
+		startDate: new Date("2024-11-22"),
+		endDate: new Date("2024-11-28"),
+		color: colors.timeOfInsight,
+	},
+	{
+		title: "🍀 Looter's Fortune (Realm Fragments)",
+		startDate: new Date("2024-11-29"),
+		endDate: new Date("2024-12-05"),
+		color: colors.lootersFortune,
 	},
 ];
