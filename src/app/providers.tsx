b8b588e067ea014dce029fbe89fbb988740d1420
Marco Antolini <marco.antolini2001@gmail.com>
2024-11-05 05:07:50 +0100

    add analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { CookiesProvider } from "next-client-cookies/server";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import ConvexClientProvider from "./convex-client-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ConvexClientProvider>
 			<CookiesProvider>
-				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-					<SidebarProvider>{children}</SidebarProvider>
-				</ThemeProvider>
+				<NuqsAdapter>
+					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+						<SidebarProvider>{children}</SidebarProvider>
+					</ThemeProvider>
+				</NuqsAdapter>
 			</CookiesProvider>
 		</ConvexClientProvider>
 	);
